06cb94033606896771506a2a4fc82f53e5aebc02
Bing-W <396975997@qq.com>
2018-05-29 09:18:20 +0800

    the particle system

---
+++ week7/Assets/change.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class change : MonoBehaviour {
+
+    ParticleSystem exhaust;
+    float size = 5f;
+
+    // Use this for initialization
+    void Start () {
+        exhaust = GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        size = size * 0.999f;
+        var main = exhaust.main;
+        main.startSize  = size;
+	}
+}
---
+++ week7/README.md
@@ -0,0 +1,7 @@
+# README
+
+视频的地址如下：
+
+原粒子：https://pan.baidu.com/s/1rpg87Qjztj5fPX_RilzHUg
+
+代码控制下变化的粒子：https://pan.baidu.com/s/10de_a1iMmNtSUiZLnlScYQ
