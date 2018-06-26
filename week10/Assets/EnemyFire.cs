f7959963e067fb8306437bd627457461d7a60b9b
Bing-W <396975997@qq.com>
2018-06-26 19:41:41 +0800

    多人网络游戏

---
+++ week10/Assets/EnemyFire.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EnemyFire : NetworkBehaviour
+{
+
+    public GameObject bulletPrefab;
+    // Use this for initialization
+    private int num = 0;
+	
+	// Update is called once per frame
+	void Update () {
+
+        num++;
+        num = num % 100;
+
+        if(num == 0) CmdFire();
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        // This [Command] code is run on the server!
+
+        // create the bullet object locally
+        var bullet = (GameObject)Instantiate(
+             bulletPrefab,
+             transform.position - transform.forward,
+             Quaternion.identity);
+
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward * 4;
+
+        //behind
+        var bullet1 = (GameObject)Instantiate(
+             bulletPrefab,
+             transform.position + transform.forward,
+             Quaternion.identity);
+
+        bullet1.GetComponent<Rigidbody>().velocity = transform.forward * 4;
+
+        //left
+       var bullet2 = (GameObject)Instantiate(
+             bulletPrefab,
+             transform.position - transform.right,
+             Quaternion.identity);
+
+        bullet2.GetComponent<Rigidbody>().velocity = -transform.right * 4;
+
+        //right
+        var bullet3 = (GameObject)Instantiate(
+             bulletPrefab,
+             transform.position + transform.right,
+             Quaternion.identity);
+
+        bullet3.GetComponent<Rigidbody>().velocity = transform.right * 4;
+
+        // spawn the bullet on the clients
+        NetworkServer.Spawn(bullet);
+        NetworkServer.Spawn(bullet1);
+        NetworkServer.Spawn(bullet2);
+        NetworkServer.Spawn(bullet3);
+
+        // when the bullet is destroyed on the server it will automaticaly be destroyed on clients
+        Destroy(bullet, 2.0f);
+        Destroy(bullet1, 2.0f);
+        Destroy(bullet2, 2.0f);
+        Destroy(bullet3, 2.0f);
+    }
+}
---
+++ week10/README.md
@@ -0,0 +1,5 @@
+# README
+
+本周作业是多人游戏对战，在课堂练习的基础上进行修改
+
+视频地址是：https://pan.baidu.com/s/1OQaRo9jOSFas9TdxeoXJpQ
